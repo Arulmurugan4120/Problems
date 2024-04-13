f11c1c00042cb412e147357a4fff6d2444b0ff02
ARULMURUGAN.N <131396201+Arulmurugan4120@users.noreply.github.com>
2024-04-13 20:22:58 +0530

    Create Check Prime.java

---
+++ Check Prime.java
@@ -0,0 +1,15 @@
+//https://www.naukri.com/code360/problems/check-prime_624934
+public class Solution {
+	public static String isPrime(int num) {
+		//Your code goes here
+		if(num==1){
+			return "NO";
+		}
+		for(int i=2;i*i<num;i++){
+			if(num%i==0){
+				return "NO";
+			}
+		}
+		return "YES";
+	}
+}
