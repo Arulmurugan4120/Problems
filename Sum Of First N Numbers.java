04517a3d2d05840fb5df7de90a915497ad50a4fb
ARULMURUGAN.N <131396201+Arulmurugan4120@users.noreply.github.com>
2024-04-21 21:00:05 +0530

    Create Sum Of First N Numbers.java

---
+++ Sum Of First N Numbers.java
@@ -0,0 +1,9 @@
+//https://www.naukri.com/code360/problems/sum-of-first-n-numbers_8876068
+public class Solution {
+    public static long sumFirstN(long n) {
+        // Write your code here.
+        long add=0;
+        add=n*(n+1)/2;
+        return add;
+    }
+}
