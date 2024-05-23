7b1539714e1a5ffd2c7cf30fcdcfda65f636b85b
ARULMURUGAN.N <131396201+Arulmurugan4120@users.noreply.github.com>
2024-05-23 14:58:51 +0530

    Create Find Peak Element.java

---
+++ Find Peak Element.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/find-peak-element/
+class Solution{
+    public int findPeakElement(int[] nums){
+        int first = 0;
+        int last =nums.length-1;
+        while(first<last){
+            int mid= first+(last-first)/2;
+            if(nums[mid]>nums[mid+1]){
+                last=mid;
+            }
+            else{
+                first=mid+1;
+            }
+        }
+        return first;
+    }
+}
