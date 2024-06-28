31085ef457cf67fa051f9b2c2d9f6c95680cc737
ARULMURUGAN.N <natrajarul2016@gmail.com>
2024-06-28 21:21:22 +0530

    Create Self Dividing Numbers.java

---
+++ Self Dividing Numbers.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/self-dividing-numbers/description/
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = left;i <= right;i++)
+        {
+            if(check(i)){
+                arr.add(i);
+            }
+        }
+        return arr;
+        
+    }
+    boolean check(int num)
+    {
+         int n = num;
+        int count = 0; 
+        while(num > 0)
+        {
+            int rem = num%10;
+            if(rem == 0)
+            {
+                return false;
+            }
+            if(n % rem != 0)
+            {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
+    }
+}
